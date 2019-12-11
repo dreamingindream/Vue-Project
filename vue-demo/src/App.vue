89a28b2193da3e65a44eafef9ba2b85634c48669
Jiawei <liujiaweidreamer@gmail.com>
2019-12-11 22:30:31 +0800

    add xxoo-game to a real vue project.

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="xxoo" class="center">
+    <p v-if="result" style="text-align: center;"> {{ result }} </p>
+    <div class="line">
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 0)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 1)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 2)"></xxoo-unit>          
+    </div>
+    <div class="line">
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 3)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 4)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 5)"></xxoo-unit>          
+    </div>
+    <div class="line">
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 6)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 7)"></xxoo-unit>
+        <xxoo-unit :count="count" @click-unit="clickUnit($event, 8)"></xxoo-unit>          
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import XxooUnit from './components/XxooUnit.vue'
 
 export default {
-  name: 'app',
+  name: 'xxoo',
   components: {
-    HelloWorld
+    'xxoo-unit': XxooUnit
+  },
+    data() {
+    return {
+      count: 0,
+      valueMap: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ],
+      result: ""
+    }
+  },
+  methods: {
+    clickUnit(value, id) {
+      0 === this.count ? this.count = 1 : this.count = 0;
+      // console.log(value, id);
+      this.valueMap[Math.floor(id / 3)][id % 3] = value;
+      this.tell();
+    },
+    tell() {
+      for (let i = 0; i < 3; i++) {
+        if (
+          null !== this.valueMap[i][0] &&
+          this.valueMap[i][0] === this.valueMap[i][1] &&
+          this.valueMap[i][1] === this.valueMap[i][2]
+        ) {
+          this.result = this.valueMap[i][0];
+        }
+      }
+      for (let i = 0; i < 3; i++) {
+        if (
+          null !== this.valueMap[0][i] &&
+          this.valueMap[0][i] === this.valueMap[1][i] &&
+          this.valueMap[1][i] === this.valueMap[2][i]
+        ) {
+          this.result = this.valueMap[0][i];
+        }
+      }
+      if (
+        null !== this.valueMap[1][1] &&
+        (this.valueMap[0][0] === this.valueMap[1][1] &&
+          this.valueMap[1][1] === this.valueMap[2][2]) ||
+        (this.valueMap[0][2] === this.valueMap[1][1] &&
+          this.valueMap[1][1] === this.valueMap[2][0])
+      ) {
+        this.result = this.valueMap[1][1];
+      }
+
+      if (null !== this.result && "" !== this.result) this.result += "赢了！";
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .line {
+    display: flex;
+    justify-content: center;
+  }
 </style>
---
+++ vue-demo/src/components/XxooUnit.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="unit" @click="clickUnit">
+      <template v-if="clicked"> {{ value }} </template>
+      <template v-else></template>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'XxooUnit',
+  data() {
+    return {
+      clicked: false,
+      value: ""
+    }
+  },
+  props: ["count"],
+  methods: {
+    clickUnit: function () {
+      if (this.clicked) return;
+      this.clicked = true;
+      this.value = 0 === this.count ? "x" : "O";
+      this.$emit('click-unit', this.value);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .unit {
+    border: 1px solid black;
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    font-size: 50px;
+    line-height: 100px;
+    text-align: center;
+  }
+</style>
